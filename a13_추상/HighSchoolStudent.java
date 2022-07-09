29a6d49ab1ac0096a87f57f8b18aab69f4edcb9a
ChoiHunJe <103643691+ChoiHunJE@users.noreply.github.com>
2022-07-09 12:22:51 +0900

    Delete a13_추상 directory

--- a13_추상/HighSchoolStudent.java
+++
@@ -1,8 +0,0 @@
-package a13_추상;
-
-public class HighSchoolStudent extends Student {
-	@Override
-	public void canNotSmoking() {
-		System.out.println("고등학생은 흡연 불가");
-	}
-}
--- a13_추상/Person.java
+++
@@ -1,6 +0,0 @@
-package a13_추상;
-
-public abstract class Person {
-	public abstract void work();
-	public abstract void eatSomething(); 
-}
